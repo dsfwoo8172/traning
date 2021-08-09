814cf36a5d494260dfdee6e9692eb0f138d25e1d
方紹軒 <houshouken@fangshaoxuans-MacBook-Pro.local>
2021-08-09 20:36:35 +0800

    test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,45 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController do
-  let(:random_user){ build(:random_user) }
   describe "Get new" do
-    it "response is success" do
+    
+    before do
       get :new
+    end
+
+    it "response is success" do
       expect(response).to be_successful
     end
 
     it "render new is success" do
-      get :new
       expect(response).to render_template(:new)
     end
 
     it "status 200 is success" do
-      get :new
       expect(response).to have_http_status(200)
     end
   end
 
   describe "Post create" do
+    let(:user){ create(:random_user) }
+
     it "Login user is success" do
-      user = create(:random_user)
       post :create, params: { email: user.email, password: user.password }
       expect(response).to redirect_to root_path
     end
 
     it "redirect is success" do
-      user = create(:random_user)
       post :create, params: { email: user.email, password: user.password }
       expect(response).to have_http_status(302)
     end
 
     it "Login user without email is fail" do
-      user = create(:random_user)
       post :create, params: { password: user.password }
       expect(response).to render_template(:new)
     end   
     
     it "Login user without password is fail" do
-      user = create(:random_user)
       post :create, params: { email: user.email }
       expect(response).to render_template(:new)
     end 
