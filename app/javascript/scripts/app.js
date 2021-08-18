94251d7f67513bbffa383410a9da194254cd9b0c
方紹軒 <houshouken@fangshaxuansMBP.lan>
2021-08-18 17:56:09 +0800

    fix

--- app/javascript/scripts/app.js
+++ app/javascript/scripts/app.js
@@ -3,7 +3,7 @@ const { get } = require("jquery")
 window.addEventListener('turbo:load', () => {
   // 打開 new task modal 清空裡面的 value
   $('#new_task_btn').on('click', function(){
-    // $('#task_title').val('')
+    $('#task_title').val('')
     $('#task_tag_list').val(null).trigger('change')
   })
 })
